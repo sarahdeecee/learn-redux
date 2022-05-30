6ce423df77c23a81f6d181fc415051ebc6569849
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-05-30 16:21:41 -0400

    change increment and decrement to change by a given number

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ function App() {
   const counter = useSelector(state => state.counter);
   const isLogged = useSelector(state => state.isLogged);
   const dispatch = useDispatch();
-
+  const num = 5;
+  
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
-      <button onClick={() => dispatch(increment())}>+</button>
-      <button onClick={() => dispatch(decrement())}>-</button>
+      <div>
+        Counter number: {num}
+      </div>
+      <div>
+        <button onClick={() => dispatch(increment(num))}>+</button>
+        <button onClick={() => dispatch(decrement(num))}>-</button>
+      </div>
 
       <button onClick={() => dispatch(login())}>{isLogged ? 'Logout' : 'Login'}</button>
       {isLogged ? <h3>Valuable Information I shouldn't see</h3> : ''}
