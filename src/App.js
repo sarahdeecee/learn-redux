4fc7e0bc4c21df2a47db628aeedaa7fd4782d101
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-05-30 16:11:59 -0400

    implement increment, decrement, login/logout actions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement, login} from './actions';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>App</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+
+      <button onClick={() => dispatch(login())}>{isLogged ? 'Logout' : 'Login'}</button>
+      {isLogged ? <h3>Valuable Information I shouldn't see</h3> : ''}
     </div>
   );
 }
