a77cc648caa6cea5edbfdeac60c487552d741f0a
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-05-30 15:11:23 -0400

    create reducers in separate files

--- src/index.js
+++ src/index.js
@@ -4,41 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
+import allReducer from './reducers';
 
-// STORE -> GLOBALIZED STATE
-
-
-// ACTION (describes what you want to do) (func that returns obj)
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
-
-// REDUCER (describes how your actions transform one state to another)
-const counter = (state = 0, action) => {
-  switch(action.type){
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
-}
-
-let store = createStore(counter);
-
-// Display it in the console
-store.subscribe(() => console.log(store.getState()));
-
-// DISPATCH (send action to reducer, store gets updated)
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
+const store = createStore(allReducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import counterReducer from './counter';
+import loggedReducer from './isLogged';
+import { combineReducers } from 'redux';
+
+const allReducer = combineReducers({
+  counter: counterReducer,
+  isLogged: loggedReducer
+})
+export default allReducers;
